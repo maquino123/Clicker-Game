a45b7d8b212acdf28dde36d21723c7085db20491
Marlon Aquino <marlon.aquino24@gmail.com>
2021-02-09 14:19:51 -0800

    add button for trading gold

--- IncrementalGame/main.js
+++ IncrementalGame/main.js
@@ -1,6 +1,9 @@
 var game = {
 	mangoes: 0,
 	totalMangoes: 0,
+	gold: 0,
+	goldCost: 100,
+	totalGold: 0,
 	totalClicks: 0,
 	clickValue: 1,
 	version: 0.000,
@@ -9,6 +12,9 @@ var game = {
 			this.mangoes += amount; //this = whatever variable in var game
 			this.totalMangoes += amount;
 			display.updateScore();
+			if (game.mangoes >= 50){ //Display trade button once user reaches a certain amount of mangoes
+				document.getElementById("tradeButton").style.display = 'inline-block';
+		 }
 	},
 
 	getScorePerSecond: function() {
@@ -17,14 +23,27 @@ var game = {
 				scorePerSecond += items.income[i] * items.count[i];
 		}
 		return scorePerSecond;
+	},
+
+	tradeForGold: function(){
+		//First check if you have enough mangoes to trade for Gold
+		if (game.mangoes >= game.goldCost){
+			game.mangoes -= game.goldCost;
+			game.gold += 1;
+			game.goldCost = Math.round(game.goldCost * 1.15);
+			document.getElementById("gold").style.display = 'inline-block';
+			document.getElementById("gold").innerHTML = "Gold:" + game.gold;
+			document.getElementById("goldCost").innerHTML = game.goldCost;
+		}
 	}
 };
 
+//Literally just have to add to this list to add more resources
 var items = {
 	name: [
 			"Equipment",
 			"Farmer",
-			"Sprinkler"
+			"Sprinkler",
 		],
 
 	image: [
@@ -62,7 +81,6 @@ var items = {
 		}
 };
 
-
 var display = {
 	updateScore: function() {
 		document.getElementById("mangoes").innerHTML = game.mangoes;
@@ -74,8 +92,8 @@ var display = {
 		for (i = 0; i < items.name.length; i++){
 			document.getElementById("storeContainer").innerHTML += '<table class="store" onClick="items.purchase('+i+')"><tr><td id="image"><img src='+items.image[i]+'></td> <td id="nameandcost"><p>'+items.name[i]+'</p><p><span>'+items.cost[i]+'</span> Mangoes</p></td><td id="amount"><span>'+items.count[i]+'</span></td></tr></table>';
 		}
-	}
-};
+	},
+}
 
 window.onload = function() {
 	display.updateScore();
